12ba29d3177dbe9e7d9d7680a7bf46cb35d0857b
milezperhour <hyperbolvoid@gmail.com>
2018-07-25 19:24:55 -0700

    added more payload functionality

--- chapter6/burp_fuzzing/bhp_fuzzer.py
+++ chapter6/burp_fuzzing/bhp_fuzzer.py
@@ -26,3 +26,74 @@ def getGeneratorName(self):
 # IIntruderPayloadGenerator class, which is called BHPFuzzer
 def createNewInstance(self, attack):
     return BHP(self, attack)
+
+
+# BHPFuzzer class that extends IIntruderPayloadGenerator
+class BHPFuzzer(IIntruderPayloadGenerator):
+    def __init__(self, extender, attack):
+        self._extender     = extender
+        self._helpers      = extender._helpers
+        self._attack       = attack
+        seld.max_payloads  = 10
+        self.num_iterations = 0
+
+        return
+
+
+# checks when maximum number of fuzzing iterations is reached
+def hasMorePayloads(self):
+    if self.num_iterations == self.max_payloads:
+        return False
+    else:
+        return True
+
+
+# receives the original HTTP payload; fuzzing happens here
+# current_payload variable arrives as a byte array
+def getNextPayload(self, current_payload):
+
+    # convert byte array into a string
+    payload = ''.join(chr(x) for x in current_payload)
+
+    # call our simple mutator to fuzz the POST
+    payload = self.mutate_payload(payload)
+
+    # increase the number of fuzzing attempts
+    self.num_iterations += 1
+
+    return payload
+
+
+def reset(self):
+    self.num_iterations = 0
+    return
+
+
+def mutate_payload(self, original_payload):
+    # pick a simple mutator or even call a script
+    picker = random.randint(1, 3)
+
+    # select a random offset in the payload to mutate
+    offset  = random.randint(0, len(original_payload)-1)
+    payload = original_payload[:offset]
+
+    # random offset insert a SQL injection attempt
+    if picker == 1:
+        payload += "'"
+
+    # jam an XSS attempt in
+    if picker == 2:
+        payload += "<script>alert('BHP!');</script>"
+
+    # repeat a chunk of the original payload a random number
+    if picker == 3:
+        chunk_length = random.randint(len(payload[offset:]),len(payload)-1)
+        repeater     = random.randint(1,10)
+
+        for i in range(repeater):
+            payload += original_payload[offset:offset+chunk_length]
+
+    # add the remaining bits of the payload
+    payload += original_payload[offset:]
+
+    return payload

9484e94ecdfd214d57d93aee2fc308a2008cf98e
milezperhour <hyperbolvoid@gmail.com>
2018-07-25 18:22:31 -0700

    added comments

--- chapter6/burp_fuzzing/bhp_fuzzer.py
+++ chapter6/burp_fuzzing/bhp_fuzzer.py
@@ -1,4 +1,4 @@
-from burp import IBurpExtender
+from burp import IBurpExtender # import the IBurpExtender class (required)
 from burp import IIntruderPayloadGeneratorFactory
 from burp import IIntruderPayloadGenerator
 
@@ -6,19 +6,23 @@ from java.util import List, ArrayList
 
 import random
 
+# define BurpExtender class, which extends the IBurpExtender and IIntruderPayloadGeneratorFactory classes
 class BurpExtender(IBurpExtender, IIntruderPayloadGeneratorFactory):
     def registerExtenderCallbacks(self, callbacks):
         self._callbacks = callbacks
         self._helpers   = callbacks.getHelpers()
 
+        # function to register the class so the Intruder tool is aware that
+        # we can generate payloads
         callbacks.registerIntruderPayloadGeneratorFactory(self)
 
         return
 
-
+# function to return the name of the payload generator
 def getGeneratorName(self):
     return 'BHP Payload Generator'
 
-
+# function that receives the attack parameter and returns an instance of the
+# IIntruderPayloadGenerator class, which is called BHPFuzzer
 def createNewInstance(self, attack):
     return BHP(self, attack)

a5e735609727c9eab9f2569336f011d9439ceeac
milezperhour <hyperbolvoid@gmail.com>
2018-08-04 16:34:39 -0700

    added Menu and Search functions

--- chapter6/bhp_bing.py
+++ chapter6/bhp_bing.py
@@ -30,3 +30,38 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
         menu_list = ArrayList()
         menu_list.add(JMenuItem('Send to Bing', actionPerformed=self.bing_menu))
         return menu_list
+
+    # this function is triggered when user clicks the context menu that we defined
+    def bing_menu(self, event):
+        # grab the details of what the user clicked
+        http_traffic = self.content.getSelectedMessages()
+
+        print '%d requests highlighted' % len(http_traffic)
+
+        for traffic in http_traffic:
+            http_service = traffic.getHttpService()
+            host         = http_service.getHost()
+
+            print 'User selected host: %s' % host
+            self.bing_search(host)
+
+        return
+
+
+    def bing_search(self, host):
+        # check if we have an IP or hostname
+        is_ip = re.match('[0-9]+(?:\.[0-9]+){3}', host)
+
+        if is_ip:
+            ip_address = host
+            domain     = False
+        else:
+            ip_address = socket.gethostbyname(host)
+            domain     = True
+
+        bing_query_string = "'ip:%s'" % ip_address
+        self.bing_query(bing_query_string)
+
+        if domain:
+            bing_query_string = "'domain:%s'" % host
+            self.bing_query(bing_query_string)

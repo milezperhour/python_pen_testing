d6cbc5860824fd6a9be6f6f1a64ffc6463bf4e3b
milezperhour <hyperbolvoid@gmail.com>
2018-08-04 18:08:59 -0700

    added mangle and display fuunctions with commentary

--- chapter6/bhp_wordlist.py
+++ chapter6/bhp_wordlist.py
@@ -56,3 +56,77 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
         menu_list.add(JMenuItem('Create Wordlist', actionPerformed = self.wordlist_menu))
 
         return menu_list
+
+
+# Defines the menu-click handler
+def wordlist_menu(self, event):
+    # grab the details of what the user clicked
+    http_traffic = self.context.getSelectedMessages()
+
+    for traffic in http_traffic:
+        http_service = traffic.getHttpService()
+        host         = http_service.getHost()
+
+        # saves the name of the responding host
+        self.hosts.add(host)
+
+        http_response = traffic.getResponse()
+
+        if http_response:
+            #retrieves the HTTP response and feeds it to get_words function
+            self.get_words(http_response)
+
+    self.display_wordlist()
+    return
+
+
+def get_words(self, http_response):
+    # splits out the header from the message body
+    headers, body = http_response.tostring().split('\r\n\r\n', 1)
+
+    # skip the non-text responses
+    if headers.lower().find('content-type: text') == -1:
+        return
+
+    # TagStripper class strips the HTML code from the rest of the page text
+    tag_stripper = TagStripper()
+    page_text    = tag_stripper.strip(body)
+    # regular expression; find all words starting with an alphabetic character
+    # followed by 2 or more 'word' characters
+    words        = re.finall('[a-zA-Z]\w{2,}', page_text)
+
+    for word in words:
+        # filter out long strings
+        if len(word) <= 12:
+            # successful words are saved in lowercase to the wordlist
+            self.wordlist.add(word.lower())
+
+    return
+
+# Mangles and displays captured words
+# Takes a base word and turns it into a number of password guesses based on
+# common password creation 'strategies'
+def mangle(self, word):
+    # create a list of suffixes to tack on the end of the base word (including current year)
+    year     = datetime.now().year
+    suffixes = ['', '1', '!', year]
+    mangled = []
+
+    # loop through attempt (with capital version of base word for good measure)
+    for password in (word, word.capitalize()):
+        for suffix in suffixes:
+            mangled.append('%s%s' % (password, suffix))
+
+    return mangled
+
+
+def display_wordlist(self):
+    # 'John the Ripper'-style comment to print which sites were used to generate this wordlist
+    print '#!comment: BHP Wordlist for sites(s) %s' % ', '.join(self.hosts)
+
+    # mangle each base word and print the results
+    for word in sorted(self.wordlist):
+        for password in self.mangle(word):
+            print password
+
+    return

